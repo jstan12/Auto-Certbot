244bada995d3074c5d47a3bea6c7c9e215d0e7aa
Joe Stanulis <jstanulis@protonmail.com>
2019-07-10 15:47:45 -0400

    Update renew-cert.sh

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source conf
-leDir="/etc/letsencrypt/live/${DAG_URL}"
+leDir="/etc/letsencrypt/live/${SERVER_URL}"
 homeDir="/home/${USER}"
 
-ssh $USER@$DAG_URL "[ "$(docker ps -a | grep access-gateway)" ] && docker stop access-gateway;certbot renew;sudo cp $leDir/privkey.pem $homeDir/privkey.pem;sudo cp $leDir/cert.pem $homeDir/cert.pem"
-scp $USER@$DAG_URL:$homeDir/\{cert.pem,privkey.pem} $DOWNLOAD
-ssh $USER@$DAG_URL "rm $homeDir/{cert.pem,privkey.pem};[ "$(docker ps -a | grep access-gateway)" ] && docker start access-gateway"
+ssh $USER@$SERVER_URL "[ "$(docker ps -a | grep access-gateway)" ] && docker stop access-gateway;certbot renew;sudo cp $leDir/privkey.pem $homeDir/privkey.pem;sudo cp $leDir/cert.pem $homeDir/cert.pem"
+scp $USER@$SERVER_URL:$homeDir/\{cert.pem,privkey.pem} $DOWNLOAD
+ssh $USER@$SERVER_URL "rm $homeDir/{cert.pem,privkey.pem};[ "$(docker ps -a | grep access-gateway)" ] && docker start access-gateway"
