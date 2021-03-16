27b14bed81b4ff16b516695c22daa765fcb837b5
Joe Stanulis <46797963+jstan12@users.noreply.github.com>
2021-03-16 15:57:18 -0400

    Update new-cert.sh

--- new-cert.sh
+++ new-cert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source conf
-leDir="/etc/letsencrypt/live/${SERVER_URL}"
-homeDir="/home/${USER}"
+domain="${DOMAIN}"
+leDir="/etc/letsencrypt/live/${DOMAIN}"
+homeDir="/home/${USER}
+mail="${MAIL}"
 
-ssh $USER@$SERVER_URL "[ "$(docker ps -a | grep access-gateway)" ] && docker stop access-gateway;sudo apt-get install software-properties-common;sudo add-apt-repository ppa:certbot/certbot;sudo apt-get update;sudo apt install certbot -y;sudo certbot certonly --standalone --agree-tos --register-unsafely-without-email --no-eff-email --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges http -d $SERVER_URL;sudo cp $leDir/privkey.pem $homeDir/privkey.pem;sudo cp $leDir/cert.pem $homeDir/cert.pem"
-scp $USER@$SERVER_URL:$homeDir/\{cert.pem,privkey.pem} $DOWNLOAD
-ssh $USER@$SERVER_URL "rm $homeDir/{cert.pem,privkey.pem};[ "$(docker ps -a | grep access-gateway)" ] && docker start access-gateway"
+sudo apt update && sudo apt install letsencrypt software-properties-common -y
+sudo certbot certonly --manual --preferred-challenges=dns --email $mail --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d $domain -d *.$domain
+sudo cp $leDir/privkey.pem $homeDir/privkey.pem;sudo cp $leDir/cert.pem $homeDir/cert.pem
